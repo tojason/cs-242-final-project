f81a29848defbcff0916ee081edfc58549e748a9
Jason <jason@232.128-25.236.17.192.in-addr.arpa>
2017-12-04 05:01:42 -0600

    update mongoose protocol

--- server.js
+++ server.js
@@ -9,12 +9,24 @@ const cookieParser = require('cookie-parser');
 
 app.use(express.static('./public/'));
 
+// Allow CORS so that backend and frontend could be put on different servers
+var allowCrossDomain = function (req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "X-Requested-With, X-HTTP-Method-Override, Content-Type, Accept");
+    res.header("Access-Control-Allow-Methods", "POST, GET, PUT, DELETE, OPTIONS");
+    next();
+};
+app.use(allowCrossDomain);
+
 app.use(bodyParser.urlencoded({
   extended: true
 }));
 app.use(bodyParser.json());
 
 // Static routes
+app.route('/').get(function(req, res) {
+  return res.sendFile(path.join(__dirname, './source/html/index.html'));
+});
 app.route('/SignUp').get(function(req, res) {
   return res.sendFile(path.join(__dirname, './source/html/index.html'));
 });
